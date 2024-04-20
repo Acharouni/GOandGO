68efcf77ac7fb5b04898761a192764350d79393a
Acharouni <aymencharouni@outlook.fr>
2024-04-20 15:56:49 +0100

    Gopage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_and_go/screen/splash_screen.dart';
+import 'package:go_and_go/screen/startScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: splashScreen(),
+      home: startScreen(),
     );
   }
 }
